3e89d55856ea8df615f1ed1038955380ac2f5a89
Keerthana Yellapragada <keerthanay@gmail.com>
2022-06-01 15:27:06 -0700

    dragon spec

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,31 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragonsInput) {
+    let dragonArray = [];
+    console.log(dragonsInput)
+
+    for (let dragon of dragonsInput) {
+      if (dragon instanceof Dragon) {
+        dragonArray.push(dragon.name)
+      }
+    }
+
+    return dragonArray;
+  }
+
+}
+
+
+module.exports = Dragon;
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
